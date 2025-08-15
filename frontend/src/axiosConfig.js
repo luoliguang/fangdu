b548949371fc588d5b65130154625134c515a842
luoliguang <2298928961@qq.com>
2025-08-15 17:35:43 +0800

    修复axiosConfig

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -6,11 +6,15 @@ const apiClient = axios.create({
 });
 
 // 请求拦截器
+// 它的作用就是在每次请求发送前，都检查一下本地有没有令牌，有的话就带上
 apiClient.interceptors.request.use(
     (config) => {
+        // 1. 从 localStorage 获取令牌
         const token = localStorage.getItem('accessToken');
+
+        // 2. 如果令牌存在
         if (token) {
-            // 如果存在通行证，就在请求头里带上
+            // 3. 将令牌放入请求头，格式必须是 'Bearer ' + token
             config.headers['Authorization'] = `Bearer ${token}`;
         }
         return config;
